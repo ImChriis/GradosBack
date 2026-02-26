d5780cc439caaec9ccf940019077fcb6139d557f
Chris <christophermedinabarroso@gmail.com>
2026-02-26 16:54:21 -0400

    cambios act contracts

--- src/modules/actContracts/actContractsController.js
+++ src/modules/actContracts/actContractsController.js
@@ -2,11 +2,23 @@ const db = require('../../config/db');
 
 exports.getActs = async (req, res) => {
     try{
-        const sql = 'SELECT a.CodigoActo, ag.Fecha, ag.Hora, ag.siglas, ag.especialidad, la.TxLugar FROM dbo.ActosGrados AS ag INNJER JOIN dbo.LugarActo AS la On ag.CoLugar = la.CoLugar INNER JOIN dbo.Instituciones as i ON ag.CodigoInst = i.CodigoInst WHERE ag.Culminada = "0" ORDER BY ag.CodigoActo DESC';
+        const sql = 'SELECT ag.CodigoActo, ag.Fecha, ag.Hora, ag.siglas, ag.especialidad, la.TxLugar FROM actosgrados AS ag INNER JOIN lugaracto AS la On ag.CoLugar = la.CoLugar INNER JOIN instituciones as i ON ag.CodigoInst = i.CodigoInst WHERE ag.Culminada = "0" ORDER BY ag.CodigoActo DESC';
         const [rows] = await db.query(sql);
         res.json(rows);
     } catch (error){
         console.error('Error fetching acts:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+exports.getActsUsersByCodigoActo = async (req, res) => {
+    const { CodigoActo } = req.params;
+    try{
+        const sql = 'SELECT NoContrato, Nombre, MnPagado, MnSaldo from deactosgrados where CodigoActo = ? ORDER BY Nombre ASC';
+        const [rows] = await db.query(sql, [CodigoActo]);
+        res.json(rows);
+    }catch (error){
+        console.error('Error fetching acts users:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
--- src/modules/actContracts/actContractsRoutes.js
+++ src/modules/actContracts/actContractsRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const actContractsController = require('./actContractsController');
 
-router.get('/', actContractsController.getActContracts);
-router.post('/add', actContractsController.addActContract);
-router.put('/update/:CoContrato', actContractsController.updateActContract);
+router.get('/', actContractsController.getActs);
+router.get('/:CodigoActo/users', actContractsController.getActsUsersByCodigoActo);
+// router.post('/add', actContractsController.addActContract);
+// router.put('/update/:CoContrato', actContractsController.updateActContract);
 
 module.exports = router;
