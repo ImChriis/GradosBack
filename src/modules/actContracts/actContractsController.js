8f30557568d54118e2580e9a63629aee1c78dd5c
Chris <christophermedinabarroso@gmail.com>
2026-02-27 15:31:57 -0400

    cambios

--- src/modules/actContracts/actContractsController.js
+++ src/modules/actContracts/actContractsController.js
@@ -2,7 +2,7 @@ const db = require('../../config/db');
 
 exports.getActs = async (req, res) => {
     try{
-        const sql = 'SELECT ag.CodigoActo, ag.Fecha, ag.Hora, ag.siglas, ag.especialidad, la.TxLugar FROM actosgrados AS ag INNER JOIN lugaracto AS la On ag.CoLugar = la.CoLugar INNER JOIN instituciones as i ON ag.CodigoInst = i.CodigoInst WHERE ag.Culminada = "0" ORDER BY ag.CodigoActo DESC';
+        const sql = 'SELECT ag.CodigoActo, ag.Fecha, ag.Hora, ag.siglas, ag.especialidad, ag.titulo, la.TxLugar, i.CodigoInst, i.nbInstitucion FROM actosgrados AS ag INNER JOIN lugaracto AS la On ag.CoLugar = la.CoLugar INNER JOIN instituciones as i ON ag.CodigoInst = i.CodigoInst WHERE ag.Culminada = "0" ORDER BY ag.CodigoActo DESC';
         const [rows] = await db.query(sql);
         res.json(rows);
     } catch (error){
@@ -10,7 +10,7 @@ exports.getActs = async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
-
+ 
 exports.getActsUsersByCodigoActo = async (req, res) => {
     const { CodigoActo } = req.params;
     try{
